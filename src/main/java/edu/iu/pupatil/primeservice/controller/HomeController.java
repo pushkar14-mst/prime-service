69ef001601b746de5551dfc173c4a57482491902
Pushkar Patil <pushkar@Pushkars-MacBook-Pro.local>
2024-02-27 18:30:58 -0500

    allowed cross origin access

--- src/main/java/edu/iu/pupatil/primeservice/controller/HomeController.java
+++ src/main/java/edu/iu/pupatil/primeservice/controller/HomeController.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
-
+@CrossOrigin(origins="*")
 public class HomeController {
     @GetMapping
     public String greetings() {
